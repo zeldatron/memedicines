4edc1e7548abb8f9299bb0a3ce55946e18ffb358
ZeldaLin <zeldatron@gmail.com>
2018-03-08 17:06:55 -0800

    text styles

--- wp-content/themes/mamaterra/styles/components/organisms/footer.php
+++ wp-content/themes/mamaterra/styles/components/organisms/footer.php
@@ -1,15 +1,9 @@
 <!-- components/organisms/footer.php -->
-
+<div id="location">
+    
+</div>
 <footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mamaterra' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'mamaterra' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'mamaterra' ), 'mamaterra', '<a href="https://github.com/CodeRodeo/cr-starter-theme">Code Rodeo</a>' );
-			?>
+            ©2018 Mother Earth Medicines
 		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+</footer><!-- #colophon -->
